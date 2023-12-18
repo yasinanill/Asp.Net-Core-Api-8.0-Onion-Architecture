0b495c6e063d7a1620961bfe2f99b79295687c4c
yasin kaya <74669908+yasinanill@users.noreply.github.com>
2023-12-18 21:46:28 +0300

    Initial commit

---
+++ CarBook.Domain/Entities/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class Service
+    {
+        public int ServiceID { get; set; }
+        public int Title { get; set; }
+        public int Description { get; set; }
+        public int IconUrl { get; set; }
+    }
+}
